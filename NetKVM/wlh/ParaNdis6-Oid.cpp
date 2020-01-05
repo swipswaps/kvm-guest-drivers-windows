557b9df2f42f36ae7f127ed0e1befec4ab79faf5
Yuri Benditovich <yuri.benditovich@daynix.com>
2020-01-05 11:29:40 +0200

    netkvm: report correct revision of NDIS_PM_CAPABILITIES
    Starting from NDIS 6.30 the driver can report revision 2 of
    this structure. On NDIS 6.80 the driver must do that. We keep
    the structure as is for Win 8.1 build and current Win 10 build.
    For future builds for NDIS 6.50+ we will report revision 2.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/wlh/ParaNdis6-Oid.cpp
+++ NetKVM/wlh/ParaNdis6-Oid.cpp
@@ -1243,7 +1243,11 @@ void ParaNdis6_Fill620PowerCapabilities(PNDIS_PM_CAPABILITIES pPower620Caps)
     pPower620Caps->Header.Type = NDIS_OBJECT_TYPE_DEFAULT;
     pPower620Caps->Header.Revision = NDIS_PM_CAPABILITIES_REVISION_1;
     pPower620Caps->Header.Size = NDIS_SIZEOF_NDIS_PM_CAPABILITIES_REVISION_1;
-
+#if NDIS_SUPPORT_NDIS650
+    // can be used also for 6.30, but let's keep it backward compatible
+    pPower620Caps->Header.Revision = NDIS_PM_CAPABILITIES_REVISION_2;
+    pPower620Caps->Header.Size = NDIS_SIZEOF_NDIS_PM_CAPABILITIES_REVISION_2;
+#endif
     // part of WOL support
     pPower620Caps->SupportedWoLPacketPatterns = 0;
     pPower620Caps->NumTotalWoLPatterns = 0;
